9b574c22481b7289d8ff40861a2a97b1e8e0e51f
enniszeu <enniszeu@gmail.com>
2020-01-16 08:57:44 +0700

    loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Banner from './Components/header/Banner';
+import Loading from './Components/loader/Loading';
 
 class App extends React.Component{
     render(){
@@ -8,6 +9,7 @@ class App extends React.Component{
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <Loading />
                         <Banner />
                     </div>
                 </div>
